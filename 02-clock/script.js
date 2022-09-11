9997a2fa1d4521f272e4c6e262e19c909cdf4fd8
Mike <spormuv@gmail.com>
2022-09-12 02:01:24 +0300

    feat: add functionality

---
+++ 02-clock/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegree = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+  minHand.style.transform = `rotate(${minsDegree}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegree = (hours / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hoursDegree}deg)`;
+
+  console.log(`${hours} : ${mins} : ${seconds}`);
+}
+
+setInterval(setDate, 1000);
