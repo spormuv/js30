9c7256b3e21026d6dc43e5755b4bdbee9faf2e8f
Mike <spormuv@gmail.com>
2022-11-03 21:10:09 +0300

    init: start webcam project

---
+++ 19-webcam-fun/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// consts
+
+const video = document.querySelector('.player');
+const canvas = document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+//get video
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => console.error(err.message));
+}
+
+getVideo();
